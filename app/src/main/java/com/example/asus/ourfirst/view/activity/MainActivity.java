4f358e52aa1498d28f5658118679326adae91acc
gq <1057386657@qq.com>
2018-01-23 17:01:19 +0800

    two

--- app/src/main/java/com/example/asus/ourfirst/view/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/ourfirst/view/activity/MainActivity.java
@@ -12,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.i("", "onCreate: ");
         setContentView(R.layout.activity_main);
+
+               Log.i("见覅家居服你该板块","快回复第的女");
 
     }
 }
--- app/src/main/java/com/example/asus/ourfirst/view/viewcallback/Iview.java
+++ app/src/main/java/com/example/asus/ourfirst/view/viewcallback/Iview.java
@@ -5,4 +5,5 @@ package com.example.asus.ourfirst.view.viewcallback;
  */
 
 public interface Iview {
+
 }
