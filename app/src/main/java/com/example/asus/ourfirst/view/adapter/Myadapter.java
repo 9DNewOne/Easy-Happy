e23e5fe8f049d809a49ea085988eb8298d95d793
zhangrui19941025 <1833175648@qq.com>
2018-01-24 20:30:16 +0800

    推荐

--- app/src/main/java/com/example/asus/ourfirst/view/adapter/Myadapter.java
+++
@@ -1,8 +0,0 @@
-package com.example.asus.ourfirst.view.adapter;
-
-/**
- * Created by asus on 2018/1/24.
- */
-
-public class Myadapter {
-}
--- app/src/main/java/com/example/asus/ourfirst/view/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/asus/ourfirst/view/fragment/RecommendFragment.java
@@ -3,12 +3,16 @@ package com.example.asus.ourfirst.view.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.asus.ourfirst.R;
+import com.example.asus.ourfirst.utils.otherutils.TabUtils;
+import com.example.asus.ourfirst.view.adapter.RecommendPagerAdaoter;
 
 /**
  *  热门推荐
@@ -16,12 +20,38 @@ import com.example.asus.ourfirst.R;
 public class RecommendFragment extends Fragment {
 
 
+    private View view;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recommend, container, false);
+        view = inflater.inflate(R.layout.fragment_recommend, container, false);
 
 
         return view;
 
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final TabLayout mTab=view.findViewById(R.id.Tab);
+
+        ViewPager mRecommendVP=view.findViewById(R.id.RecommendPager);
+
+        RecommendPagerAdaoter pagerAdaoter = new RecommendPagerAdaoter(getFragmentManager());
+
+        mRecommendVP.setAdapter(pagerAdaoter);
+
+        mTab.setupWithViewPager(mRecommendVP);
+
+        mTab.post(new Runnable() {
+            @Override
+            public void run() {
+                TabUtils.setIndicator(mTab,50,50);
+            }
+        });
+
+    }
+
 
 }
